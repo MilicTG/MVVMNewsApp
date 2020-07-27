ae79808b8760aca074a1385ba04bce3067a8aa32
MilicTG <milic.tg@gmail.com>
2020-07-27 12:19:34 -0700

    Added retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object {
+        const val API_KEY = "c81bca9e45ba4aeaba8c50cfc80651a1"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
