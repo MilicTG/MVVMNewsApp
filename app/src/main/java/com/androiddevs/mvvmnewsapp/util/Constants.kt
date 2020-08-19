afdd3358da7111158e566dec54f13610ce85f52d
MilicTG <milic.tg@gmail.com>
2020-08-19 13:43:36 +0200

    Finished video 4

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "c81bca9e45ba4aeaba8c50cfc80651a1"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
