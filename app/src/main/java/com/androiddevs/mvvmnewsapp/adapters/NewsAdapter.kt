8deedcd21ff9839cc05ce8d99939657ce0d492ee
MilicTG <milic.tg@gmail.com>
2020-08-20 17:16:46 +0200

    Finished video 6

--- app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsAdapter.kt
@@ -1,10 +1,15 @@
 package com.androiddevs.mvvmnewsapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.models.Article
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_article_preview.view.*
 
 class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
@@ -13,26 +18,57 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
     //Create Callback for DiffUtil
     private val differCallback = object : DiffUtil.ItemCallback<Article>() {
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
-            //compare old new 
+            //compare old with new
             return oldItem.url == newItem.url
         }
 
+        //compere the context of old and new item
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
-            TODO("Not yet implemented")
+            return oldItem == newItem
         }
 
     }
 
+    //async list differ
+    //takes two list and compares them and check the differences
+    //executes async
+    private val differ = AsyncListDiffer(this, differCallback)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        TODO("Not yet implemented")
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_article_preview, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(ivArticleImage)
+            tvSource.text = article.source.name
+            tvTitle.text = article.title
+            tvDescription.text = article.description
+            tvPublishedAt.text = article.publishedAt
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
+        }
     }
 
+    //there is no list
+    //differ detriments the list
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return differ.currentList.size
+    }
+
+    //click listener
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    //gets called from outside
+    fun setOnItemClickListener(listener: (Article) -> Unit){
+        onItemClickListener = listener
     }
 
     inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
